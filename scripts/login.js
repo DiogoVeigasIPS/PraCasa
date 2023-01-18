ec59f68ee1209b50fde30d089583d5c2bf71e2c6
DiogoVeigasIPS <120383436+DiogoVeigasIPS@users.noreply.github.com>
2023-01-18 21:22:29 +0000

    Add files via upload

--- scripts/login.js
+++ scripts/login.js
@@ -1,3 +1,5 @@
+
+
 function mostrarDiv(div, caret){//mostrar/enconder div com direito a setinha a mudar de direção
     var element = document.getElementById(div);
     if (element.style.display === 'none') {
@@ -15,8 +17,71 @@ function capitalizeFirstLetter(string){//deixar o primeiro caracter maiúsculo
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-function login (){//captura o nome de um email
+let showErrorMessage = (div, message) => {
+    var parentElement = document.getElementById(div);//encontra o div de erro
+
+    var errorParagraph = document.createElement("p");//cria paragrafo
+    var error = document.createTextNode(message);//cria text node
+    errorParagraph.appendChild(error);//mete text node no paragrafo
+
+    parentElement.appendChild(errorParagraph);//mete paragrafo no div
+    parentElement.style.display = "block";//apresenta o erro
+}
+
+let clearPassword = () => {//limpar palavra passe
+    document.getElementById("password").focus();
+    document.getElementById("password").value = "";
+}
+
+let focusEmail = () => {//foco no email
+    document.getElementById("email").focus();
+}
+
+let clearDiv = (div) => {//limpa todos os paragrafos do div (o resto tambem, mas so sao adicionados paragrafos)
+    while (div.firstChild) {
+        div.removeChild(div.firstChild);
+    }
+}
+
+let displayAllNone = (div, div2) =>{//esconder erros passados
+    div.style.display = "none";
+    div2.style.display = "none";
+}
+
+let clearErrors = () => {
+    var divEmail = document.getElementById("emailError");
+    var divPassword = document.getElementById("passwordError");
+
+    clearDiv(divEmail);//limapr divs
+    clearDiv(divPassword);
+    displayAllNone(divEmail, divPassword);//esconde-los
+}
+
+function login(){//Entrar sessao
     var email = document.getElementById("email").value;
-    var name = email.split('@')[0];
-    localStorage.setItem("name", capitalizeFirstLetter(name));
+    var password = document.getElementById("password").value;//dados que o utilizador submeteu
+
+    clearErrors();
+
+    if(email == "" || password == ""){
+        showErrorMessage("passwordError", "Deve preencher todos os campos!");
+        return;
+    }
+
+    var user = localStorage.getItem(email);//pesquisar na local storage
+    if(user != null){//se existir
+        if(password == user){//se a senha e o email coincidirem
+            var name = email.split('@')[0];//nome ate ao @
+            localStorage.setItem("name", capitalizeFirstLetter(name));//guarda o nome com a primeira letra maiuscula
+
+            alert("Sessão iniciada com sucesso!");
+            window.location.href = "index.html";//redirecionado ao inicio
+        }else{
+            clearPassword();
+            showErrorMessage("passwordError", "Palavra-passe incorreta!");
+        }
+    }else{//se nao existir
+        focusEmail();       
+        showErrorMessage("emailError", "Uma conta com esse email não existe!");
+    }
 }
---
+++ scripts/register.js
@@ -0,0 +1,101 @@
+let clearPasswords = () => {//limpar os campos de palavra passe
+    document.getElementById("password").focus();
+    document.getElementById("password").value = "";
+    document.getElementById("password2").value = "";
+}
+
+let clearEmail = () => {//limpar os campos do email
+    //document.getElementById("email").value = "";
+    document.getElementById("email").focus();
+}
+
+function hasNumeric(string) {//tem numero?
+    for(var i = 0; i < string.length; i++){
+        if(string.charAt(i) > 0 && string.charAt(i) < 9){//verificar se o caractere entra entre os digitos 0 e 9
+            return 1;
+        }
+    }
+    return 0;
+}
+
+function hasUpperCase(string) {//tem maiúsucla
+    for(var i = 0; i < string.length; i++){
+        if(string.charAt(i) == string.charAt(i).toUpperCase()){//verificar se o caractere é igual à sua versão maiúsucla
+            return i;
+        }
+    }
+    return 0;
+}
+
+let displayAllNone3 = (div, div2, div3) =>{//esconde divs
+    div.style.display = "none";
+    div2.style.display = "none";
+    div3.style.display = "none";
+}
+
+let clearErrors3 = () => {//remove os erros antigos
+    var divEmail = document.getElementById("emailError");
+    var divPassword = document.getElementById("passwordError");
+    var divPassword2 = document.getElementById("password2Error");
+
+    clearDiv(divEmail);//remove paragrafos
+    clearDiv(divPassword);
+    clearDiv(divPassword2);
+    displayAllNone3(divEmail, divPassword, divPassword2);
+}
+
+
+let register = () =>{
+    email = document.getElementById("email").value;
+    password = document.getElementById("password").value;
+    password2 = document.getElementById("password2").value;//dados do utilizador
+
+    clearErrors3();
+
+    if(email == "" || password == "" || password2 == ""){
+        showErrorMessage("password2Error", "Deve preencher todos os campos!");
+        return;
+    }
+
+    var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;//regex de verificacao de email (roubado)
+
+    if(!email.match(validRegex)){
+        clearEmail();
+        showErrorMessage("emailError", "Email inválido!");
+        return;
+    }
+
+    if(localStorage.getItem(email) != null){//verifica se ja existe alguma conta com tal email
+        clearEmail();
+        showErrorMessage("emailError", "Uma conta com esse email já existe!");
+        return;
+    }
+
+    if(password != password2){
+        clearPasswords();
+        showErrorMessage("password2Error", "Palavras-passe não coincidem!");
+        return;
+    }
+
+    if(password.length < 5){
+        clearPasswords();
+        showErrorMessage("passwordError", "Palavra passe possui menos de 5 caracteres!");
+        return;
+    }
+
+    if(hasNumeric(password) == 0){//verifica se tem numero
+        clearPasswords();
+        showErrorMessage("passwordError", "Palavra passe não possui números!");
+        return;
+    }
+
+    if(hasUpperCase(password) != 0){//verifica se tem maiuscula
+        clearPasswords();
+        showErrorMessage("passwordError", "Palavra passe não possui maiúsculas!");
+        return;
+    }
+
+    localStorage.setItem(email, password);//cria uma conta nova caso nenhum erro ocorra
+    alert("Conta registada com sucesso!");
+    window.location.href = "login.html";//redirecionar para login
+}
